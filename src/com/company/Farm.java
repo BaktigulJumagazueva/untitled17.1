52f82ab83ca22f8224ee98ff74726c71b8889108
13 <baktyguljumagazyeva@gmail.com>
2022-08-23 13:34:59 +0600

    TT 17.1

--- src/com/company/Farm.java
+++ src/com/company/Farm.java
@@ -1,2 +1,75 @@
-package com.company;public class Farm {
+package com.company;
+
+import java.util.Arrays;
+
+public class Farm {
+    private String adress;
+    private Cow[] cows;
+    private Horse[] horses;
+    private Sheep[] sheep;
+    private String overName;
+
+    public Farm(String adress, Cow[] cows, Horse[] horses, Sheep[] sheep, String overName) {
+        this.adress = adress;
+        this.cows = cows;
+        this.horses = horses;
+        this.sheep = sheep;
+       this. overName = overName;
+    }
+public Farm(String adress,String overName){
+        this.adress=adress;
+        this.overName= overName;
+
+}
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public Cow[] getCows() {
+        return cows;
+    }
+
+    public void setCows(Cow[] cows) {
+        this.cows = cows;
+    }
+
+    public Horse[] getHorses() {
+        return horses;
+    }
+
+    public void setHorses(Horse[] horses) {
+        this.horses = horses;
+    }
+
+    public Sheep[] getSheep() {
+        return sheep;
+    }
+
+    public void setSheep(Sheep[] sheep) {
+        this.sheep = sheep;
+    }
+
+    public String getOverName() {
+        return overName;
+    }
+
+    public void setOverName(String overName) {
+       this. overName = overName;
+    }
+
+    @Override
+    public String toString() {
+        return "Farm{" +"\n"+
+                "adress=" + adress + '\'' +"\n"+
+                " cows=" + Arrays.toString(cows) +"\n"+
+                " horses=" + Arrays.toString(horses) +"\n"+
+                " sheep=" + Arrays.toString(sheep) +"\n"+
+                " overName=" + overName + '\'' +"\n"+
+                '}';
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,38 @@
+package com.company;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Sheep sheep1 = new Sheep(35, 2, "female", "Lia");
+        Sheep sheep2 = new Sheep(40, 3, "male", "Lack");
+        Sheep sheep3 = new Sheep(33, 1, "female", "Lili");
+        Horse horse1 = new Horse(150, 8, "male", "Chack");
+        Horse horse2 = new Horse(140, 7, "female", "Cha");
+        Cow cow1 = new Cow(160, 9, "female", "Mia");
+        Cow cow2 = new Cow(150, 9, "male", "Miagi");
+        Cow cow3 = new Cow(170, 9, "female", "Sacha");
+        Cow cow4 = new Cow(163, 9, "male", "Uma");
+        Cow cow5 = new Cow(168, 10, "male", "Fardi");
+
+        Sheep[] sheep = {sheep1, sheep2, sheep3};
+        Horse[] horses = {horse1, horse2};
+        Cow[] cows = {cow1, cow2, cow3, cow4, cow5};
+        Farm farm = new Farm("Kirova", cows,horses,sheep,"Poliana");
+        System.out.println(farm);
+
+
+
+
+        Cow cow = new Cow(185, 5, "female", "ziro");
+        Horse horse = new Horse(200, 9, "male", "Kondor");
+        Sheep sheep6 = new Sheep(65, 4, "female", "Tuzbig");
+        Sheep[] sheeps= {sheep6};
+        Horse[] horsess = {horse};
+        Cow[] cowss = {cow};
+
+        Farm farm2= new Farm("Maevka 12",cowss,horsess,sheeps,"Ekomeat");
+        System.out.println(farm2);
+
     }
-}
+    }
